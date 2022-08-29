e90df1053ffadbbf4b35f6c55c9f2a3cf613b827
higorasilverio <higorasilverio@outlook.com>
2022-08-29 09:35:08 -0300

    🔐 (firebaseConfig): token secrets renewed and hidden

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBe9Z7Ak7av79Y7s5bK6EmS1DZVY7gVnvg",
-  authDomain: "next-simple-crud.firebaseapp.com",
-  projectId: "next-simple-crud",
-  storageBucket: "next-simple-crud.appspot.com",
-  messagingSenderId: "768456231728",
-  appId: "1:768456231728:web:44670d0c188a2f925356f5",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
